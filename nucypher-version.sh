2df1b5d4954b9a9ff344492b397b630b08f19c65
Sergey <52304443+c29r3@users.noreply.github.com>
2020-02-14 01:15:03 +0300

    Fix bug with parsing local version of NU

--- nucypher-version.sh
+++ nucypher-version.sh
@@ -3,13 +3,14 @@
 METRIC="$1"
 USR=`find /home/*/.local/share/nucypher/ -type f -name ursula.json | awk -F "/" '{print $3}'`
 if [ -z "$USR" ]; then
-   HOME="/root"
+   HOME1="/root"
 else
-   HOME="/home/$USR"
+   HOME1="/home/$USR"
 fi
 ##### RUN #####
-NUCYPHERABOUTDIR=`find  $HOME/ -name "nucypher" -ls 2>/dev/null | egrep -o  "/(home || root).*site-packages/nucypher"`
-NUCYPHERVERSIONLOCAL=`cat $NUCYPHERABOUTDIR/__about__.py | grep "__version__ =" | egrep -o '".*' | tr -d '"'`
+NUCYPHERVERSIONLOCAL=`cat $(find  $HOME1/ -user $USR -type f -name "__about__.py" -ls 2>/dev/null | grep site-packages/nucypher | egrep -o "/.*") | grep "__version__ =" \ 
+ | egrep -o '".*' | tr -d '"'`
+
 NUCYPHERVERSIONGIT=`curl -s "https://api.github.com/repos/nucypher/nucypher/tags" | jq -r '.[0].name' | tr -d 'v'`
 if [ "${METRIC}" = "nucypherversionlocal" ]; then
   echo $NUCYPHERVERSIONLOCAL
