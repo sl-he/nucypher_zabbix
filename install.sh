cd63a53642787b7dbfe264751ec33541f7e9d6c4
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-15 03:19:50 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+##### SCRIPT PARAMETERS #####
+UPDATEFILEPATH=`find /home/*/ -type f -name git_update.sh`
+if [ -z "$UPDATEFILEPATH" ]; then
+   UPDATEFILEPATH=`find /root/ -type f -name git_update.sh`
+else
+   HOME1=`find /home/*/ -type f -name git_update.sh`
+fi
+crontab -l | { cat; echo "15 0 * * * $UPDATEFILEPATH"; } | crontab -
 mkdir -p /etc/zabbix/scripts
 cp ~/nucypher_zabbix/*.sh /etc/zabbix/scripts/
 cp ~/nucypher_zabbix/zabbix_agentd.conf /etc/zabbix/
