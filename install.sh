1ab0c63f95eddc31914bb97f808683c9fdaa2584
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-15 00:19:39 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p /etc/zabbix/scripts
 cp ~/nucypher_zabbix/*.sh /etc/zabbix/scripts/
+cp ~/nucypher_zabbix/zabbix_agentd.conf /etc/zabbix/
 chmod 755 /etc/zabbix/scripts/*.sh
 service zabbix-agent restart
