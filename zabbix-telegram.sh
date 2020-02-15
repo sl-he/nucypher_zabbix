2da501b4436b71d5a5d32e2415fbe3a382d1f573
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-15 16:40:27 +0500

    Create zabbix-telegram.sh

---
+++ zabbix-telegram.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Replace $BOT_TOKEN to your REAL bot token
+TOKEN='$BOT_TOKEN'
+if [ $# -ne 3 ] ; then echo 'FAIL: Params not defined.' && echo 'Usage: zabbix-telegram.sh TelegramID Subject Message' && exit 1 ; fi
+CHAT_ID="$1"
+SUBJECT="$2"
+MESSAGE="$3"
+curl -s --header 'Content-Type: application/json' --request 'POST' --data "{\"chat_id\":\"${CHAT_ID}\",\"text\":\"${SUBJECT}\n${MESSAGE}\"}" "https://api.telegram.org/bot${TOKEN}/sendMessage" | grep -q '"ok":false,'
+if [ $? -eq 0 ] ; then exit 1 ; fi
