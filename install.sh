9e50827c4f29b651b69e487ebde70d45198b3e68
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-15 18:53:51 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 ##### SCRIPT PARAMETERS #####
-cd ~ && git clone https://github.com/sl-he/nucypher_zabbix.git
-UPDATEFILEPATH=`find /home/*/ -type f -name git_update.sh`
-if [ -z "$UPDATEFILEPATH" ]; then
-   UPDATEFILEPATH=`find /root/ -type f -name git_update.sh`
-else
-   HOME1=`find /home/*/ -type f -name git_update.sh`
-fi
 USR=`find /home/*/.local/share/nucypher/ -type f -name ursula.json | awk -F "/" '{print $3}'`
 if [ -z "$USR" ]; then
    USR1="root"
@@ -14,7 +7,6 @@ else
    USR1=$USR
 fi
 crontab -l | { cat; echo "*/5 * * * * su $USR1 -c 'cd ~ && source nucypher-venv/bin/activate && nucypher status stakers --provider ~/.ethereum/goerli/geth.ipc --network cassandra > /tmp/nucypher.tmp && cp /tmp/nucypher.tmp /tmp/nucypher.txt'"; } | crontab -
-crontab -l | { cat; echo "1 0 * * * $UPDATEFILEPATH"; } | crontab -
 mkdir -p /etc/zabbix/scripts
 curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/geth.sh > /etc/zabbix/scripts/geth.sh
 curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/nucypher-stats.sh > /etc/zabbix/scripts/nucypher-stats.sh
