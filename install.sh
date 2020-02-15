304677784db2b7062d961e9d40819604f480847d
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-15 17:28:56 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,13 @@ if [ -z "$UPDATEFILEPATH" ]; then
 else
    HOME1=`find /home/*/ -type f -name git_update.sh`
 fi
+USR=`find /home/*/.local/share/nucypher/ -type f -name ursula.json | awk -F "/" '{print $3}'`
+if [ -z "$USR" ]; then
+   USR1="root"
+else
+   USR1=$USR
+fi
+crontab -l | { cat; echo "*/5 * * * * su $USR1 -c 'cd ~ && source nucypher-venv/bin/activate && nucypher status stakers --provider ~/.ethereum/goerli/geth.ipc --network cassandra > /tmp/nucypher.tmp && cp /tmp/nucypher.tmp /tmp/nucypher.txt'"; } | crontab -
 crontab -l | { cat; echo "1 0 * * * $UPDATEFILEPATH"; } | crontab -
 mkdir -p /etc/zabbix/scripts
 curl -s https://raw.githubusercontent.com/sl-he/nucypher_zabbix/master/geth.sh > /etc/zabbix/scripts/geth.sh
