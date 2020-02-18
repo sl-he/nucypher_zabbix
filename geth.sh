7c8bc0b07ab5a521272a38be9af0a12710bed90a
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-18 18:13:05 +0500

    Update geth.sh

--- geth.sh
+++ geth.sh
@@ -10,10 +10,6 @@ fi
 IPC="$HOME1/.ethereum/goerli/geth.ipc"
 WORKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .worker_address| tr -d '"'`
 STAKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .checksum_address| tr -d '"'`
-
-echo $WORKERADDRESS
-echo $STAKERADDRESS
-
 ##### RUN #####
 BLOCKHEIGHT=`geth --exec "eth.blockNumber" attach $IPC`
 if [ "$BLOCKHEIGHT" = 0 ]; then
@@ -24,9 +20,6 @@ WORKERETHBALANCE=`echo $WEIWORKERETHBALANCE / 1000000000000000000 | bc -l`
 sleep 1
 WEISTAKERETHBALANCE=`curl -s 'https://api-goerli.etherscan.io/api?module=account&action=balance&address='$STAKERADDRESS'&tag=latest' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36' | jq .result | tr -d '"'`
 STAKERETHBALANCE=`echo $WEISTAKERETHBALANCE / 1000000000000000000 | bc -l`
-echo $BLOCKHEIGHT
-echo $WORKERETHBALANCE
-echo $STAKERETHBALANCE
 if [ "${METRIC}" = "gethblockheight" ]; then
   echo $BLOCKHEIGHT
 fi
