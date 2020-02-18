fe7be69b84fae44cdea8dc323a94b5e2529cb792
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-18 17:59:06 +0500

    Update geth.sh

--- geth.sh
+++ geth.sh
@@ -9,17 +9,31 @@ else
 fi
 IPC="$HOME1/.ethereum/goerli/geth.ipc"
 WORKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .worker_address| tr -d '"'`
+STAKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .checksum_address| tr -d '"'`
+
+echo $WORKERADDRESS
+echo $STAKERADDRESS
+
 ##### RUN #####
 BLOCKHEIGHT=`geth --exec "eth.blockNumber" attach $IPC`
 if [ "$BLOCKHEIGHT" = 0 ]; then
   BLOCKHEIGHT=`geth --exec eth.syncing attach $IPC | jq .currentBlock`
 fi
-WEIETHBALANCE=`curl -s 'https://api-goerli.etherscan.io/api?module=account&action=balance&address='$WORKERADDRESS'&tag=latest' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36' | jq .result | tr -d '"' `
-WORKERETHBALANCE=`echo $WEIETHBALANCE / 1000000000000000000 | bc -l`
+WEIWORKERETHBALANCE=`curl -s 'https://api-goerli.etherscan.io/api?module=account&action=balance&address='$WORKERADDRESS'&tag=latest' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36' | jq .result | tr -d '"'`
+WORKERETHBALANCE=`echo $WEIWORKERETHBALANCE / 1000000000000000000 | bc -l`
+sleep 1
+WEISTAKERETHBALANCE=`curl -s 'https://api-goerli.etherscan.io/api?module=account&action=balance&address='$STAKERADDRESS'&tag=latest' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36' | jq .result | tr -d '"'`
+STAKERETHBALANCE=`echo $WEISTAKERETHBALANCE / 1000000000000000000 | bc -l`
+echo $BLOCKHEIGHT
+echo $WORKERETHBALANCE
+echo $STAKERETHBALANCE
 if [ "${METRIC}" = "gethblockheight" ]; then
   echo $BLOCKHEIGHT
 fi
 if [ "${METRIC}" = "workerethbalance" ]; then
   echo $WORKERETHBALANCE
+fi
+if [ "${METRIC}" = "stakerethbalance" ]; then
+  echo $STAKERETHBALANCE
 fi
 exit 0
