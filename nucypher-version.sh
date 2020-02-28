fa9265c754f057bce212c846c148fda225b05630
Andrey <61023562+sl-he@users.noreply.github.com>
2020-02-28 18:02:52 +0500

    Update nucypher-version.sh

--- nucypher-version.sh
+++ nucypher-version.sh
@@ -10,7 +10,6 @@ fi
 ##### RUN #####
 NU_ABOUT_PATH=`find  $HOME1/ -user $USR -type f -name "__about__.py" -ls 2>/dev/null | grep site-packages/nucypher | egrep -o "/.*"`
 NUCYPHERVERSIONLOCAL=`cat $NU_ABOUT_PATH | grep "__version__ =" | egrep -o '".*' | tr -d '"'`
-
 NUCYPHERVERSIONGIT=`curl -s "https://api.github.com/repos/nucypher/nucypher/tags" | jq -r '.[0].name' | tr -d 'v'`
 if [ "${METRIC}" = "nucypherversionlocal" ]; then
   echo $NUCYPHERVERSIONLOCAL
