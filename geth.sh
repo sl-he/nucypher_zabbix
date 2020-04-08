bacd3cc9fb9616b6706bae558d9c18e5fd659988
Andrey <61023562+sl-he@users.noreply.github.com>
2020-04-08 15:00:46 +0500

    Update geth.sh

--- geth.sh
+++ geth.sh
@@ -7,18 +7,9 @@ if [ -z "$USR" ]; then
 else
    HOME1="/home/$USR"
 fi
-IPC="$HOME1/.ethereum/goerli/geth.ipc"
 WORKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .worker_address| tr -d '"'`
 STAKERADDRESS=`cat $HOME1/.local/share/nucypher/ursula.json | jq .checksum_address| tr -d '"'`
 ##### RUN #####
-BLOCKHEIGHT=`geth --exec "eth.blockNumber" attach $IPC`
-if [ "$BLOCKHEIGHT" = 0 ]; then
-  BLOCKHEIGHT=`geth --exec eth.syncing attach $IPC | jq .currentBlock`
-fi
-GETHPEERCOUNT=$(geth attach $IPC --exec net.peerCount)
-if [ "${METRIC}" = "gethblockheight" ]; then
-  echo $BLOCKHEIGHT
-fi
 if [ "${METRIC}" = "workerethbalance" ]; then
   WEIWORKERETHBALANCE=`curl -s 'https://api-goerli.etherscan.io/api?module=account&action=balance&address='$WORKERADDRESS'&tag=latest' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36' | jq .result | tr -d '"'`
   WORKERETHBALANCE=`echo $WEIWORKERETHBALANCE / 1000000000000000000 | bc -l`
@@ -28,8 +19,5 @@ if [ "${METRIC}" = "stakerethbalance" ]; then
   WEISTAKERETHBALANCE=`curl -s 'https://api-goerli.etherscan.io/api?module=account&action=balance&address='$STAKERADDRESS'&tag=latest' -H 'dnt: 1' -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36' | jq .result | tr -d '"'`
   STAKERETHBALANCE=`echo $WEISTAKERETHBALANCE / 1000000000000000000 | bc -l`
   echo $STAKERETHBALANCE
-fi
-if [ "${METRIC}" = "gethpeercount" ]; then
-  echo $GETHPEERCOUNT
 fi
 exit 0
